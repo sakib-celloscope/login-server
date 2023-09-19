5a64625b617f10c0c9811d0625fd2fb99139cb64
sakib-celloscope <sakib.celloscope@gmail.com>
2023-09-19 18:26:31 +0600

    login template

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const path = require('path')
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Alo Server is running.");
+});
+
+app.get('/login', (req, res) =>{
+    res.sendFile(__dirname + '/login.html');
+})
+
+app.listen(port, () => {
+  console.log(`Alo Server is running on port: ${port}`);
+});
